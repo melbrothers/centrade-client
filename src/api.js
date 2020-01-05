f6b70d590dccb4a62ed1d2231f17d493cf3ffcb8
Xin Zheng <jason.zhengxin@gmail.com>
2020-01-05 11:05:05 +1100

    add axios credential option

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://centrade-api.herokuapp.com',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/ld+json',
     'Accept': 'application/ld+json'
