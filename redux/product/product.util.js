b52a538ac82489fb7637232acd494b2087af7dcb
Xin Zheng <jason.zhengxin@gmail.com>
2020-01-05 11:37:39 +1100

    remove bearer token

--- redux/product/product.util.js
+++ redux/product/product.util.js
@@ -2,9 +2,9 @@ import api from '../../src/api';
 export const getCategoryList = async (token, pageNumber) => {
   const requestUrl = `/api/categories?page=${pageNumber}`;
   return api.get(requestUrl, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
+    // headers: {
+    //   Authorization: `Bearer ${token}`
+    // }
   });
 }
 
@@ -12,8 +12,8 @@ export const getCategoryList = async (token, pageNumber) => {
 export const getProductList = async (token, categoryfilterQuery = '', pageNumber) => {
   const requestUrl = `/api/products?${categoryfilterQuery}&page=${pageNumber}`;
   return api.get(requestUrl, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
+    // headers: {
+    //   Authorization: `Bearer ${token}`
+    // }
   });
 }
